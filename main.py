cf44316edcf1236b6b2c8cd210a8ce8464c94798
DianaKB <dianchik0719@gmail.com>
2022-04-24 00:50:18 +0300

    I change pleace of AddressBook, I add Note and NotesBook + some functional for this classes

--- main.py
+++ main.py
@@ -79,22 +79,24 @@ class NotesBook(UserDict):
         self.data = []
 
     def add_note(self, note: Note):
-        added_note = {'id': '', 'tag': '', 'note': note}
-        self.data.append(added_note)
+        self.note = note.value
+        self.added_note = {'id': '', 'tag': '', 'note': self.note}
+        self.data.append(self.added_note)
 
-    def print_menu(self): # this def will change, when we will have finish menu
+    def menu_notes(self, action): # this def will change, when we will have finish
+
         while True:
-            command = input('''Do you want create notes or check?
-                            Create: enter 1 or create
-                            Check: enter 2 or check
-                            If you need exit, enter exit or 0\n''').lower()
-            if command == '1' or command == 'create':
+            if (action == '1' or action == 'check') and self.data:
+                print(self.data)
+                break
+            elif action == '2' or action == 'create':
                 note = Note(input('Enter your notes: '))
                 self.add_note(note)
-            elif command == '2' or command == 'check':
-                print(self.data)
-            elif command == '0' or command == 'exit':
                 break
+            elif action == '5' or action == 'exit':
+                break
+            else:
+                print('You was wrong or notes didn\'t create')
 
 
 class AddressBook(UserDict):
@@ -111,19 +113,53 @@ class Menu:
         show_menu.field_names = [f"{18 * '-'}Меню{18 * '-'}"]
         show_menu.hrules = 1
         show_menu.align = "l"
-        show_menu.add_rows([["1. Створити новий контакт"],
-                            ["2. Добавити дані до існуючого контакту"],
-                            ["3. Видалити дані з контакту"],
-                            ["4. Змінити дані контакту"],
-                            ["5. Іменниники"],
-                            ["6. Посторінковий вивід контактної книги"],
-                            ["7. Пошук по контактній книзі"],
-                            ["8. Показати всі контакти"],
-                            ["9. Нотатки"],
-                            ["10. Сортувати папку"],
-                            ["11. Вихід"],
+        show_menu.add_rows([["1. Контакти"],
+                            ["2. Нотатки"],
+                            ["3. Іменниники"],
+                            ["4. Сортувати папку"],
+                            ["5. Вихід"],
                             ])
         return show_menu
+
+    @property
+    def main_contact(self):
+        show_main_contact = ColorTable(theme=Themes.OCEAN)
+        show_main_contact.field_names = [f"{18 * '-'}Контакти{18 * '-'}"]
+        show_main_contact.hrules = 1
+        show_main_contact.align = "l"
+        show_main_contact.add_rows([["1. Створити контакт"],
+                            ["2. Добавити дані до існуючого контакту"],
+                            ["3. Редагувати дані контакту"],
+                            ["4. Видалити дані з контакту"],
+                            ["5. Повернутись в попереднє меню"],
+                            ])
+        return show_main_contact
+
+    @property
+    def add_menu(self):
+        show_add_contact = ColorTable(theme=Themes.OCEAN)
+        show_add_contact.field_names = [f"{18 * '-'}Що будем добавляти?{18 * '-'}"]
+        show_add_contact.hrules = 1
+        show_add_contact.align = "l"
+        show_add_contact.add_rows([["1. Телефон"],
+                                   ["2. Емейл"],
+                                   ["3. Адресу"],
+                                   ["4. День народження"],
+                                   ["5. Повернутись в попереднє меню"]])
+        return show_add_contact
+
+    @property
+    def edit_menu(self):
+        show_edit = ColorTable(theme=Themes.OCEAN)
+        show_edit.field_names = [f"{18 * '-'}Що будем редагувати?{18 * '-'}"]
+        show_edit.hrules = 1
+        show_edit.align = "l"
+        show_edit.add_rows([["1. Телефон"],
+                              ["2. Емейл"],
+                              ["3. Адресу"],
+                              ["4. День народження"],
+                              ["5. Повернутись в попереднє меню"]])
+        return show_edit
 
     @property
     def delete_menu(self):
@@ -140,54 +176,75 @@ class Menu:
         return show_delete
 
     @property
-    def add_menu(self):
-        show_add_contact = ColorTable(theme=Themes.OCEAN)
-        show_add_contact.field_names = [f"{18 * '-'}Що будем добавляти?{18 * '-'}"]
-        show_add_contact.hrules = 1
-        show_add_contact.align = "l"
-        show_add_contact.add_rows([["1. Телефон"],
-                              ["2. Емейл"],
-                              ["3. Адресу"],
-                              ["4. День народження"],
-                              ["5. Повернутись в попереднє меню"]])
-        return show_add_contact
+    def notes_menu(self):
+        show_notes_menu = ColorTable(theme=Themes.OCEAN)
+        show_notes_menu.field_names = [f"{18 * '-'}Нотатки{18 * '-'}"]
+        show_notes_menu.hrules = 1
+        show_notes_menu.align = "l"
+        show_notes_menu.add_rows([["1. Подивитись всі нотатки"],
+                                      ["2. Додати нотатку"],
+                                      ["3. Змінити нотатку"],
+                                      ["4. Видалити нотатку"],
+                                      ["5. Повернутись в попереднє меню"]])
+        return show_notes_menu
 
-    @property
-    def change_menu(self):
-        show_change_contact = ColorTable(theme=Themes.OCEAN)
-        show_change_contact.field_names = [f"{18 * '-'}Що будем змінювати?{18 * '-'}"]
-        show_change_contact.hrules = 1
-        show_change_contact.align = "l"
-        show_change_contact.add_rows([["1. Телефон"],
-                                   ["2. Емейл"],
-                                   ["3. Адресу"],
-                                   ["4. День народження"],
-                                   ["5. Повернутись в попереднє меню"]])
-        return show_change_contact
+
+class Handler:
+    def __init__(self):
+        self.menu = Menu()
+        self.notes_book = NotesBook()
+        self.address_book = AddressBook()
+
+    def action_note(self, notes_book: NotesBook):
+        self.notes_book = notes_book
+        while True:
+            print(self.menu.notes_menu)
+            action = input("\033[34m" + "Обери потрібну команду(1-5), "
+                                         "або я спробую вгадати: ")
+            if (action == '1' or action == 'check') and  self.notes_book.data:
+                print( self.notes_book.data)
+            elif action == '2' or action == 'create':
+                note = Note(input('Enter your notes: '))
+                self.notes_book.add_note(note)
+            elif action == '5' or action == 'exit':
+                break
+            else:
+                print('You was wrong or notes didn\'t create')
+
+    def action_phone(self, address_book: AddressBook):
+        pass
+
+    def main_action(self):
+        while True:
+            print(self.menu.main_menu)
+
+            command = input("\033[34m" + "Обери потрібну команду(1-5), "
+                                         "або я спробую вгадати: ")
+            if command.lower() in ["exit", "close", "good bye", "5", "вихід", "выход"]:
+                print("Good bye!")
+                return 'break'
+            elif command.lower() in ["нотатки", "note", "notes", "2",
+                                     "замітки", "заметки"]:
+                self.action_note(self.notes_book)
+            elif command.lower() in ["phone", "номер телефона", "номер", "1"]:
+                self.action_phone(self.address_book)
 
 
 class Bot:
 
     def __init__(self):
-
-        self.addressbook = AddressBook()
-        self.notes = []  # a func add_note should create an instance of Note
-        # and add it to current list
         self.menu = Menu()  # should be changed
-
+        self.handler = Handler()
         while True:
-            print(self.menu.main_menu)
-            command = input("I am waiting for your command: ")
-
-            if command.lower() in ["exit", "close", "good bye", "11", "вихід", "выход"]:
-                print("Good bye!")
-
-            elif command == "note" or command == "14":  #number may be change
-                self.notes_book.print_menu()
+            if self.handler.main_action():
+                break
+            self.handler.main_action()
 
     def sort_files(self, file_name):
         # external sort func should be imported
         pass
+
+
 
 
 if __name__ == "__main__":
