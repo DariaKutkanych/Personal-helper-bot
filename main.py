c61baf44c77aef308d9202370d7fd2bffe2ffb0f
DianaKB <dianchik0719@gmail.com>
2022-04-25 23:55:54 +0300

    Changed Class Menu Changed Class NoteBook: def add_note def delete_note def edit_note def sort_note Changed Class Handler

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from collections import UserDict
-from sort_files import sort_folder
+from collections import UserDict, UserList
 from prettytable.colortable import ColorTable, Themes
-import datetime
 import re
 
 
@@ -11,11 +9,11 @@ class Field:
         self.__value = None
         self.value = value
 
-    def __str__(self):
-        return self.value
-
     def __repr__(self):
-        return f'{self.value}'
+        return f"{self.value}"
+
+    def __str__(self):
+        return f"{self.value}"
 
     @property
     def value(self):
@@ -23,8 +21,7 @@ class Field:
 
     @value.setter
     def value(self, value):
-        if type(value) == str:
-            self.__value = value
+        self.__value = value
 
 
 class Name(Field):
@@ -33,36 +30,40 @@ class Name(Field):
 
 class Phone(Field):
 
-    @Field.value.setter
-    def value(self, new_value):
+    def __init__(self, phone=None):
+        self.phone = phone
+
+    @property
+    def phone(self):
+        return self.phone
+
+    @phone.setter
+    def phone(self, new_value):
         if len(list(new_value)) >= 11:
-            Field.value.fset(self, new_value)
+            self.phone = new_value
         else:
             print('\033[31m' + 'Номер не додано! Номер повинен містити не меньше 11 цифр')
             return
 
 
 class Birthday(Field):
-
-    @Field.value.setter
-    def value(self, new_value):
-        if len(list(new_value)) == 10 and \
-                int(new_value[0:4]) > 0 and \
-                int(new_value[5:7]) > 0 and \
-                int(new_value[8:10]) > 0:
-            Field.value.fset(self, new_value)
-        else:
-            print(
-                '\033[31m' + 'Некоректний формат дати! Потрібний формам ррр-мм-дд. Дата не додана')
+    pass
 
 
 class Email(Field):
 
-    @Field.value.setter
-    def value(self, add_value):
+    def __init__(self, mail=None):
+        self.email = mail
+
+    @property
+    def email(self):
+        return self.email
+
+    @email.setter
+    def email(self, add_value):
         regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
         if (re.search(regex, add_value)):
-            Field.value.fset(self, add_value)
+            self.email = add_value
         else:
             print("\033[31m" + "Емейл не доданий не коректний формат! Формат excample@mail.com")
             return
@@ -76,20 +77,46 @@ class Note(Field):
     pass
 
 
-class NotesBook(UserDict):
+class NotesBook(UserList):
 
     def __init__(self):
         self.data = []
+        self.note = ""
+        self.id_note = None
+        self.tag = ""
 
-    def add_note(self, note: Note):
-        self.note = note.value
-        self.added_note = {'id': len(self.data)+1, 'tag': '', 'note': \
-            self.note}
-        self.data.append(self.added_note)
-        flag_tag = input("Do you want to add tag for this note? Enter y, "
-                         "if yes otherwise enter n")
+    def __str__(self):
+        #need to add beautiful output like in future AddressBook
+        return f"{self.data}"
 
-    def search_note(self):
+    def add_note(self, note: Note, tag = ""):
+        self.note = note
+        self.tag = tag
+        self.id_note = len(self.data)+1
+        self.data.append({"id": self.id_note, "tag": self.tag,
+                               "note": self.note})
+
+    def delete_note(self, note: Note):
+        for i in self.data:
+            if i.get('note') == note:
+                return (f"Вы удалили заметку {self.data.pop(self.data.index(i))}")
+
+    def search_parametr_note(self, note_parametr, user_parametr):
+        for i in self.data:
+            if str(i.get(note_parametr)) == user_parametr:
+                return i.get("note")
+
+    def search_word_note(self, part_note): #need to be impoves
+        self.find_all_notes = []
+        for i in self.data:
+            if re.findall(part_note, str(i.get('note'))):
+                self.find_all_notes.append(i.get('note'))
+        return self.find_all_notes
+
+    def edit_note(self, note: Note):
+        pass
+
+    def sort_note(self):
         pass
 
 
@@ -127,7 +154,6 @@ class Menu:
                             ["4. Видалити дані з контакту"],
                             ["5. Повернутись в попереднє меню"],
                             ])
-        
         return show_main_contact
 
     @property
@@ -177,70 +203,100 @@ class Menu:
         show_notes_menu.hrules = 1
         show_notes_menu.align = "l"
         show_notes_menu.add_rows([["1. Подивитись всі нотатки"],
-                                  ["2. Додати нотатку"],
-                                  ["3. Змінити нотатку"],
-                                  ["4. Видалити нотатку"],
-                                  ["5. Повернутись в попереднє меню"]])
-        return show_notes_menu
-
-    @property
-    def delete_menu(self):
-        show_delete = ColorTable(theme=Themes.OCEAN)
-        show_delete.field_names = [f"{18 * '-'}Що будем видаляти?{18 * '-'}"]
-        show_delete.hrules = 1
-        show_delete.align = "l"
-        show_delete.add_rows([["1. Телефон"],
-                              ["2. Емейл"],
-                              ["3. Адресу"],
-                              ["4. День народження"],
-                              ["5. Видалити контакт з книги"],
-                              ["6. Повернутись в попереднє меню"]])
-        return show_delete
-
-    @property
-    def notes_menu(self):
-        show_notes_menu = ColorTable(theme=Themes.OCEAN)
-        show_notes_menu.field_names = [f"{18 * '-'}Нотатки{18 * '-'}"]
-        show_notes_menu.hrules = 1
-        show_notes_menu.align = "l"
-        show_notes_menu.add_rows([["1. Подивитись всі нотатки"],
                                       ["2. Додати нотатку"],
                                       ["3. Знайти нотатку"],
                                       ["4. Змінити нотатку"],
                                       ["5. Видалити нотатку"],
-                                      ["6. Повернутись в попереднє меню"]])
+                                      ["6. Сортувати нотатки за тегами"],
+                                      ["7. Повернутись в попереднє меню"]])
         return show_notes_menu
+
+    @property
+    def search_note(self):
+        show_edit = ColorTable(theme=Themes.OCEAN)
+        show_edit.field_names = [f"{18 * '-'}За яким критерієм?"
+                                 f"{18 * '-'}"]
+        show_edit.hrules = 1
+        show_edit.align = "l"
+        show_edit.add_rows([["1. По id замітки"],
+                              ["2. По тегу замітки"],
+                              ["3. По головному слову"],
+                              ["4. Повернутись в попереднє меню"]])
+        return show_edit
+
+    @property
+    def edit_note(self):
+        show_edit = ColorTable(theme=Themes.OCEAN)
+        show_edit.field_names = [f"{18 * '-'}Що змінити?"
+                                 f"{18 * '-'}"]
+        show_edit.hrules = 1
+        show_edit.align = "l"
+        show_edit.add_rows([["1. Тег"],
+                              ["2. нотатку"],
+                              ["3. Повернутись в попереднє меню"]])
+        return show_edit
 
 
 class Handler:
-  
-    def __init__(self):
+    def __init__(self, notes_book : NotesBook, address_book: AddressBook):
         self.menu = Menu()
-        self.notes_book = NotesBook()
-        self.address_book = AddressBook()
+        self.notes_book = notes_book
+        self.address_book = address_book
 
-    def action_note(self, notes_book: NotesBook):
+    def main_action_note(self, notes_book: NotesBook):
         self.notes_book = notes_book
         while True:
             print(self.menu.notes_menu)
             action = input("\033[34m" + "Обери потрібну команду(1-5), "
                                          "або я спробую вгадати: ")
-            if action.lower() in ["1", "check", "подивитись", "нотатки",
-                                     "замітки", "заметки"]:
-                print(self.notes_book.data)
-            elif action.lower() in ["2", "create", "створити", "создать",
-                                     "замітки", "заметки"]:
+            if action.lower() in ["1", "check", "подивитись"]:
+                print(notes_book)
+            elif action.lower() in ["2", "create", "створити", "создать"]:
                 note = Note(input('Введіть нонатку: '))
-                self.notes_book.add_note(note)
-            elif action.lower() in ["3", "знайти", "search", "нотатки",
-                                     "замітки", "заметки"]:
+                flag_tag = input("Чи хочете ви додати тег до замітки? Введіть "
+                              "так, якщо бажаєте, інакше ні: ")
+                if flag_tag.lower() in ["так", "yes", "да", "хочу"]:
+                    tag_note = Note(input('Введіть тег: '))
+                    self.notes_book.add_note(note, tag_note)
+                else:
+                    self.notes_book.add_note(note)
+            elif action.lower() in ["3", "знайти", "search", "пошук", "найти"]:
+                print(self.action_search_note(notes_book))
+            elif action.lower() in ["4", "edit", "редагувати", "змінити",
+                                    "изменить"]:
                 pass
-                #self.notes_book.add_note(note)
-            elif action.lower() in ["exit", "close", "good bye", "5", "вихід",
-                                    "выход"]:
+            elif action.lower() in ["5", "delete", "remove", "видалити",
+                                    "удалить", "стерти"]:
+                print(self.notes_book.delete_note(self.action_search_note(
+                    notes_book)))
+            elif action.lower() in ["exit", "close", "good bye", "6", "вихід",
+                                    "выход","повернутись"]:
                 break
             else:
                 print('You was wrong or notes didn\'t create')
+
+    def action_search_note(self, notes_book: NotesBook):
+        while True:
+            print(self.menu.search_note)
+
+            command = input("\033[34m" + "Обери потрібну команду(1-4), "
+                                         "або я спробую вгадати: ")
+            if command.lower() in ["id", "ид", "ід", "1"]:
+                id_parametr = input('Введіть id нотатки: ')
+                return notes_book.search_parametr_note("id", id_parametr)
+            elif command.lower() in ["tag", "тег", "notes", "2"]:
+                tag_parametr = input('Введіть tag нотатки: ')
+                return notes_book.search_parametr_note("tag", tag_parametr)
+            elif command.lower() in ["головне", "main", "слово", "3"]:
+                word_parametr = input('Введіть головне слово нотатки: ')
+                return notes_book.search_word_note(word_parametr)
+            elif command.lower() in ["exit", "close", "good bye", "4",
+                                     "вихід", "выход", "повернутись"]:
+                print("Good bye!")
+                break
+
+    def action_edit_note(self, notes_book: NotesBook):
+        pass
 
     def action_phone(self, address_book: AddressBook):
         pass
@@ -251,14 +307,12 @@ class Handler:
 
             command = input("\033[34m" + "Обери потрібну команду(1-5), "
                                          "або я спробую вгадати: ")
-
             if command.lower() in ["exit", "close", "good bye", "5", "вихід", "выход"]:
                 print("Good bye!")
                 break
-                
             elif command.lower() in ["нотатки", "note", "notes", "2",
                                      "замітки", "заметки"]:
-                self.action_note(self.notes_book)
+                self.main_action_note(self.notes_book)
             elif command.lower() in ["phone", "телефон", "номер", "1","number"]:
                 self.action_phone(self.address_book)
 
@@ -267,13 +321,19 @@ class Bot:
 
     def __init__(self):
         self.menu = Menu()  # should be changed
-        self.handler = Handler()
-        self.handler.main_action()
+        self.notes_book = NotesBook()
+        self.address_book = AddressBook()
+        self.handler = Handler(self.notes_book, self.address_book)
 
-    def sort_files(self, file_path):
-        sort_folder(file_path)
+        while True:
+            if self.handler.main_action() is None:
+                break
+            self.handler.main_action()
+
+    def sort_files(self, file_name):
+        # external sort func should be imported
+        pass
 
 
 if __name__ == "__main__":
-
     my_bot = Bot()
