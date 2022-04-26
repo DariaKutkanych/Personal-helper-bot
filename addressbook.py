5573f5605d77ceec71df6c90b1e17df39f3c8072
DariaKutkanych <d.goubenko@gmail.com>
2022-04-26 17:20:38 +0300

    restructuring

---
+++ addressbook.py
@@ -0,0 +1,25 @@
+import datetime
+from collections import UserDict
+from records import Record, Birthday, Address, Name, Phone
+
+
+class AddressBook(UserDict):
+
+    def add_record(self, set):
+        if isinstance(set, Record):
+            self.data.__setitem__(
+                set.name, (set.addresses, set.phones, set.emails, set.birthday))
+        else:
+            print("try add Record.")
+
+    def get_bd(self, day):
+        now = datetime.datetime.now()
+        delta = now + datetime.timedelta(days=day)
+        birthday_people = []
+        for k, v in self.data.items():
+            for el in v:
+                if isinstance(el, Birthday):
+                    date_val = (el.value).replace(year=now.year)
+                    if now < date_val.replace(hour=23, minute=59, second=59, microsecond=0) < delta:
+                        birthday_people.append(k)
+        print(birthday_people)
---
+++ records.py
@@ -0,0 +1,49 @@
+from field import Name, Phone, Email, Address, Birthday
+
+
+class Record:
+
+    def __init__(self, name, address=None, phone=None, email=None, birthday=None):
+        if isinstance(name, Name):
+            self.name = name
+        self.addresses = []
+        self.phones = []
+        self.emails = []
+        self.birthday = birthday
+
+    def add_address(self, address):
+        address_list = []
+        for a.value in self.addresses:
+            address_list.append(str(a.value))
+        if address in address_list:
+            print("This address alredy been added.")
+        else:
+            new_address = Address(address)
+            self.addresses.append(address)
+            print(f"for {self.name} add address {address}.")
+
+    def add_phone(self, phone):
+        phones_list = []
+        for p.value in self.phones:
+            phones_list.append(str(p.value))
+        if phone in phones_list:
+            print("This num alredy been added.")
+        else:
+            new_phone = Phone(phone)
+            self.phones.append(new_phone)
+            print(f"for {self.name} add phone {new_phone}.")
+
+    def add_mail(self, mail):
+        mail_list = []
+        for m.value in self.emails:
+            mail_list.append(str(m.value))
+        if mail in mail_list:
+            print("This mail alredy been added.")
+        else:
+            new_mail = Email(mail)
+            self.emails.append(new_mail)
+            print(f"for {self.name} add mail {mail}.")
+
+    def add_birthday(self, bd):
+        self.birthday = Birthday(bd)
+        print(f"{self.name} was born {bd}.")
