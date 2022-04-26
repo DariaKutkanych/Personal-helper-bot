c13aa4be1ed6cd7b345acdf502a8f1148fec7712
SMARTcla <misha.kononenko.2003@gmail.com>
2022-04-26 19:15:51 +0200

    Update addressbook.py

--- addressbook.py
+++ addressbook.py
@@ -23,3 +23,28 @@ class AddressBook(UserDict):
                     if now < date_val.replace(hour=23, minute=59, second=59, microsecond=0) < delta:
                         birthday_people.append(k)
         print(birthday_people)
+        
+    def search_by_name(self, surname):
+        for i in self.data.name:
+            if i.value == surname:
+                return self.data[self.data.name.index(surname)]
+        return f"There is no contact with this name"
+    
+    def search_by_phone(self, number):
+        for i in self.data.phone:
+            if i.value == number:
+                return self.data[self.data.phone.index(number)]
+        return f"There is no contact with this phone"
+    
+    def search_by_email(self, mail):
+        for i in self.data.emails:
+            if i.value == mail:
+                return self.data[self.data.emails.index(mail)]
+        return f"There is no contact with this email"
+    
+    def search_by_address(self, adres):
+        for i in self.data.addresses:
+            if i.value == adres:
+                return self.data[self.data.addresses.index(adres)]
+        return f"There is no contact with this email"
+        
