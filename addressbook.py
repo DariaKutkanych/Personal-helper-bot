f620ea01d3a115bad07f4d84d702a57bba69dbf7
KuVadym <97878308+KuVadym@users.noreply.github.com>
2022-04-27 23:36:16 +0300

    Update addressbook.py
    1. translate text
    2. change list on dict
    3. add showing date of bd (befor show just name)

--- addressbook.py
+++ addressbook.py
@@ -14,21 +14,21 @@ class AddressBook(UserDict):
 
 
     def get_bd(self, day=None):
-        if day:
+        if day.isdigit():
             now = datetime.datetime.now()
             delta = now + datetime.timedelta(days=int(day))
-            birthday_people = []
+            birthday_people = {}
             for k, v in self.data.items():
                 for el in v:
                     if isinstance(el, Birthday):
                         date_val = (el.value).replace(year=now.year)
                         if now < date_val.replace(hour=23, minute=59, second=59, microsecond=0) < delta:
-                            birthday_people.append(k)
+                            birthday_people[k] = str(date_val.date())
             print(birthday_people)
         else:
-            d = input("Enter for what period to show birthdays: ")
+            d = input("На скільки днів уперед показати іменинників?")
             self.get_bd(d)
-
+            
     def search_by_name(self, surname):
         pass
 
